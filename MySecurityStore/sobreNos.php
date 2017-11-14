90e4d6c3912afdc81ebc1dcfa5e515170a576f7c
Higor <belini.higor@gmail.com>
2017-11-14 11:12:48 -0200

    Paginas complementares

---
+++ MySecurityStore/sobreNos.php
@@ -0,0 +1,18 @@
+<?php
+include("topo.php");
+include("menu.php");
+?>
+
+<div class="container">
+	<div class="row">
+		<div class="col-md-12">
+			<h1 class="text-center">Sobre nós</h1>
+			<div class="painel">
+				<h3>My Security Store é uma empresa que tem como principal objetivo atender clientes de todos os tipos de classe econômica oferecendo seus produtos de segurança. A empresa não se responsabiliza por produtos com má instalação ou com mal uso de sua função.</h3>
+			</div>
+		</div>
+	</div>
+</div>
+<?php 
+include("rodape.php");
+?>
---
+++ MySecurityStore/televendas.php
@@ -0,0 +1,19 @@
+<?php
+
+
+include("topo.php");
+include("menu.php");
+?>
+<div class="container">
+	<div class="row">
+		<div class="col-md-12">
+			<h1 class="text-center">Televendas</h1>
+			<div class="painel">
+				<p class="text-center">Ligue agora para <span class="fa fa-phone" aria-hidden="true"></span> (35)3531-7961 e seja atendido pela equipe com o melhor preparo em televendas do país.</p>
+			</div>
+		</div>
+	</div>
+</div>
+<?php
+include("rodape.php");
+?>
