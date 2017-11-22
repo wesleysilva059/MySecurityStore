183f6f2fd21054e38897c81d2ae0d0c6731b17f8
Higor <belini.higor@gmail.com>
2017-11-22 12:33:35 -0200

    Algumas alterações

--- MySecurityStore/entrega.php
+++ MySecurityStore/entrega.php
@@ -1,94 +1,96 @@
 <?php
     session_start();
-    session_start();
     include'conexao.php';
     if (empty($_SESSION['id'])){//se usuario não está logado
         header('location:login.php');//vai até a tela de login
     }
     include("topo.php");
     include("menu.php");
+       
+   
 ?>
 <div class="container">
     <div class="col-md-12">
-        <form method="post" action="">
                 <h1 class="text-center">Formas de entrega</h1>
     </div>
     <div class="col-md-9">
                 <ul class="nav nav-pills">
-                                <li class="active"><a href="#"><i class="fa fa-truck"></i> Formas de entrega</a>
-                                </li>
-                                <li class="disabled"><a href="#"><i class="fa fa-money"></i> Formas de pagamento</a>
-                                </li>
-                                <li class="disabled"><a href="#"><i class="fa fa-eye"></i> Meus produtos</a>
-                                </li>
-                            </ul>
+                    <li class="active"><a href="#"><i class="fa fa-truck"></i> Formas de entrega</a></li>
+                    <li class="disabled"><a href="#"><i class="fa fa-money"></i> Formas de pagamento</a></li>
+                    <li class="disabled"><a href="#"><i class="fa fa-eye"></i> Meus produtos</a></li>
+                </ul>
                             <div class="margem"></div>
                             <div class="content">
-                                <div class="row">
-                                    <div class="col-sm-4 box">
-                                        <div class="text-center">
-
-                                            <h4>Sedex</h4>
-
-                                            <p>De 3 a 5 dias úteis</p>
-
-                                            <p>Valor - R$ 33,00</p>
-
-                                            <div class="text-center">
-
-                                                <input type="radio" name="" value="">
-                                            </div>
-                                        </div>
-                                    </div>
-                                    <div class="col-sm-4 box">
-                                        <div class="text-center">
-
-                                            <h4>Sedex</h4>
-
-                                            <p>De 3 a 5 dias úteis</p>
-
-                                            <p>Valor - R$ 33,00</p>
-
-                                            <div class="text-center">
-
-                                                <input type="radio" name="" value="">
-                                            </div>
-                                        </div>
-                                    </div>
-
-                                    <div class="col-sm-4 box">
-                                       <div class="text-center">
-
-                                            <h4>Sedex</h4>
-
-                                            <p>De 3 a 5 dias úteis</p>
-
-                                            <p>Valor - R$ 33,00</p>
-
-                                            <div class="text-center">
-
-                                                <input type="radio" name="" value="">
-                                            </div>
-                                        </div>
-                                    </div>
-                                </div>
                                 
+                                <div class="row">
+                                   <form name="form" method="POST">
+                                        <div class="col-sm-4 box">
+                                           <div class="text-center">
 
+                                                <h4></h4>
+
+                                                <p>De 3 a 5 dias úteis</p>
+
+                                                <p>Valor - R$ 33,00</p>
+
+                                                <div class="text-center">
+
+                                                    <input type="radio" name="opcao" value="1">
+                                                </div>
+                                            </div>
+                                        </div>
+                                        <div class="col-sm-4 box">
+                                           <div class="text-center">
+
+                                                <h4></h4>
+
+                                                <p>De 3 a 5 dias úteis</p>
+
+                                                <p>Valor - R$ 33,00</p>
+
+                                                <div class="text-center">
+
+                                                    <input type="radio" name="opcao" value="2">
+                                                </div>
+                                            </div>
+                                        </div>       
+                                        <div class="col-sm-4 box">
+                                           <div class="text-center">
+
+                                                <h4></h4>
+
+                                                <p>De 3 a 5 dias úteis</p>
+
+                                                <p>Valor - R$ 33,00</p>
+
+                                                <div class="text-center">
+
+                                                    <input type="radio" name="opcao" value="3">
+                                                </div>
+                                            </div>
+                                        </div>
+                                        <input type="submit" value="Escolher" name="formaEnvio">
+                                    </form>
+                                    <div class="margem"></div>
+                                    <tr>
+                                        <a href="index.php"><input type="submit" value="Continuar comprando" name="continuarComprando" class="button text-right"></a>
+                                        <a href="formasPagamento.php"><input type="submit" value="Ir para as formas de pagamento" name="formasPagamento" class="text-right"></a>
+                                        <a href="excluirCarrinho.php"><input type="submit" value="Cancelar compra" name="cancelarCompra" class="text-right"></a>
+                                    </tr>
+                                </div>
                             </div>
-                           <div class="margem"></div>
+                           <?php if (isset($_POST['formaEnvio'])) {
+                               
 
-                           
-                                <tr>
-                                    <a href="index.php"><input type="submit" value="Continuar comprando" name="continuarComprando" class="button text-right"></a>
-                                    <a href="formasPagamento.php"><input type="submit" value="Ir para as formas de pagamento" name="formasPagamento" class="text-right"></a>
-                                    <a href="excluirCarrinho.php"><input type="submit" value="Cancelar compra" name="cancelarCompra" class="text-right"></a>
-                                </tr>
-                       
-        </form>
-                   
-
+                           if (isset($_POST['opcao'])) {
+                               $forma = $_POST['opcao'];
+                               echo "$forma";
+                              }
+                            }
+                            ?>
 
                 </div>
+           
                 <div class="col-md-3">
                     <div class="box" id="order-summary">
                         <div>
@@ -99,18 +101,25 @@
                         <div class="table-responsive">
                             <table class="table">
                                 <tbody>
+                                    <?php foreach ($_SESSION['carrinho'] as $Codigo => $qnt) { // sessao carrinho criada anteriormente
+                                    $consulta = $conexao->query("SELECT * FROM `produtos`,`prodprecos`,prodestoque WHERE produtos.Codigo = prodprecos.idproduto AND produtos.Codigo = prodestoque.idproduto AND produtos.Codigo = '$Codigo'"); //
+                                    $exibe = $consulta->fetch(PDO::FETCH_ASSOC);
+                                    $preco = $exibe['pvenda'];
+                                    $preco = number_format($exibe['pvenda'],2,',','.');
+                                    $total = $exibe['pvenda'] *$qnt; ?>
                                     <tr>
                                         <td>Subtotal</td>
-                                        <th>R$446.00</th>
+                                        <th><strong>R$<?php echo number_format($total,2,',','.');?></strong></th>
                                     </tr>
                                     <tr>
                                         <td>Frete</td>
-                                        <th>$10.00</th>
+                                        <th>A escolher</th>
                                     </tr>
                                     <tr>
                                         <td><strong>Total</strong></td>
-                                        <th><strong>$456.00</strong></th>
+                                        <th><strong> <strong>R$<?php echo number_format($total,2,',','.');?></strong></strong></th>
                                     </tr>
+                                    <?php } ?>
                                 </tbody>
                             </table>
                         </div>
