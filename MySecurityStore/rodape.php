e38f724705f9a3f31099b6126e7d1de77db42498
cesarto <juniorcesarto@hotmail.com>
2017-11-26 19:04:08 -0200

    acessibilidade rodape.php

--- MySecurityStore/rodape.php
+++ MySecurityStore/rodape.php
@@ -6,10 +6,10 @@
                 <div class="col-md-12">
                     <div class="brand-wrapper">
                         <div class="brand-list">
-                            <a href="#"><img src="Imagens/facebookLogo.png" alt=""></a>
-                            <a href="#"><img src="Imagens/GoogleLogo.png" alt=""></a>
-                            <a href="#"><img src="Imagens/skypeLogo.png" alt=""></a>
-                            <a href="#"><img src="Imagens/acessosLogo.png" alt=""></a>
+                            <a href="https://www.facebook.com/"><img src="Imagens/facebookLogo.png" title="Acessar Facebook"></a>
+                            <a href="https://www.google.com.br/"><img src="Imagens/GoogleLogo.png" title="Acessar Google.com"></a>
+                            <a href="https://www.skype.com/pt-br/new/"><img src="Imagens/skypeLogo.png" title="Acessar Skype"></a>
+                            <a href="index.php"><img src="Imagens/acessosLogo.png" title="Nossa loja com mais de 60000 acessos."></a>
                         </div>
                     </div>
                 </div>
@@ -33,9 +33,9 @@
                             <div class="footer-about-us">
                                 <h2 class="fonte-rodape-title">A empresa</h2>
                                     <ul>
-                                        <li class="list-group-item"><a href="sobreNos.php">Quem Somos</a></li>
-                                        <li class="list-group-item"><a href="politicasPrivacidade.php">Política de Entrega e Devolução</a></li>
-                                        <li class="list-group-item"><a href="formasPagamento.php">Formas de pagamento</a></li>
+                                        <li class="list-group-item"><a href="sobreNos.php" title="Sobre nossa empresa">Quem Somos</a></li>
+                                        <li class="list-group-item"><a href="politicasPrivacidade.php" title="Política de Entrega e Devolução">Política de Entrega e Devolução</a></li>
+                                        <li class="list-group-item"><a href="formasPagamento.php" title="Formas de Pagamento">Formas de pagamento</a></li>
                                     </ul>                        
                             </div>
                         </div>
@@ -43,10 +43,10 @@
                             <div class="footer-about-us">
                                 <h2 class="fonte-rodape-title">Usuário</h2>
                                     <ul>
-                                        <li class="list-group-item"><a href="areausuario.php">Minha conta</a></li>
-                                        <li class="list-group-item"><a href="pedidos.php">Meus pedidos</a></li>
-                                        <li class="list-group-item"><a href="televendas.php">Televendas</a></li>
-                                        <li class="list-group-item"><a href="nossoEndereco.php">Contato</a></li>
+                                        <li class="list-group-item"><a href="areausuario.php" title="Minha conta">Minha conta</a></li>
+                                        <li class="list-group-item"><a href="pedidos.php" title="Meus pedidos">Meus pedidos</a></li>
+                                        <li class="list-group-item"><a href="televendas.php" title="Televendas">Televendas</a></li>
+                                        <li class="list-group-item"><a href="nossoEndereco.php" title="Contato">Contato</a></li>
                                     <ul>                        
                             </div>
                         </div>
@@ -54,11 +54,11 @@
                             <div class="footer-about-us">
                                 <h2 class="fonte-rodape-title">Categorias</h2>
                                     <ul>
-                                        <li class="list-group-item"><a href="busca.php?txtBusca=CFTV">CFTV</a></li>
-                                        <li class="list-group-item"><a href="busca.php?txtBusca=Câmera">Câmeras</a></li>
-                                        <li class="list-group-item"><a href="busca.php?txtBusca=Cabeamento">Cabeamento</a></li>
-                                        <li class="list-group-item"><a href="busca.php?txtBusca=DVR">DVRs</a></li>
-                                        <li class="list-group-item"><a href="promocoes.php">Promoções</a></li>
+                                        <li class="list-group-item"><a href="busca.php?txtBusca=CFTV" title="CFTV">CFTV</a></li>
+                                        <li class="list-group-item"><a href="busca.php?txtBusca=Câmera" title="Câmeras">Câmeras</a></li>
+                                        <li class="list-group-item"><a href="busca.php?txtBusca=Cabeamento" title="Cabeamento">Cabeamento</a></li>
+                                        <li class="list-group-item"><a href="busca.php?txtBusca=DVR" title="DVRs">DVRs</a></li>
+                                        <li class="list-group-item"><a href="promocoes.php" title="Promoções">Promoções</a></li>
                                     </ul>                        
                             </div>
                 </div>
